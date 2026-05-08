e6ead3fa7c49d868b692c2395e5b5fbfe19750fb
Jack Morris <jack.morris@sharesies.co.nz>
2026-05-09 11:31:18 +1200

    feature: effectify the worker

--- src/index.ts
+++ src/index.ts
@@ -1,39 +1,44 @@
 import { DurableObject } from "cloudflare:workers";
+import { Context, Effect, Layer } from "effect";
+import {
+	HttpRouter,
+	HttpServer,
+	HttpServerResponse,
+} from "effect/unstable/http";
+import { CfEnv } from "./services/cf-env";
 
 export class MyDurableObject extends DurableObject<Env> {
-	/**
-	 * The constructor is invoked once upon creation of the Durable Object, i.e. the first call to
-	 * 	`DurableObjectStub::get` for a given identifier (no-op constructors can be omitted)
-	 *
-	 * @param ctx - The interface for interacting with Durable Object state
-	 * @param env - The interface to reference bindings declared in wrangler.jsonc
-	 */
-	constructor(ctx: DurableObjectState, env: Env) {
-		super(ctx, env);
-	}
-
-	
 	async sayHello(name: string) {
 		return `Hello, ${name}!`;
 	}
 }
 
+const Routes = HttpRouter.addAll([
+	HttpRouter.route(
+		"GET",
+		"/:name",
+		Effect.gen(function* () {
+			const { name } = yield* HttpRouter.params;
+			const { env } = yield* CfEnv;
+
+			if (name) {
+				const stub = env.MY_DURABLE_OBJECT.getByName(name);
+				const greeting = yield* Effect.promise(() => stub.sayHello("world"));
+				return HttpServerResponse.text(greeting);
+			}
+
+			return HttpServerResponse.empty()
+
+		}),
+	),
+]);
+
+const { handler } = HttpRouter.toWebHandler(
+	Routes.pipe(Layer.provide(HttpServer.layerServices)),
+);
+
 export default {
-	/**
-	 * This is the standard fetch handler for a Cloudflare Worker
-	 *
-	 * @param request - The request submitted to the Worker from the client
-	 * @param env - The interface to reference bindings declared in wrangler.jsonc
-	 * @param ctx - The execution context of the Worker
-	 * @returns The response to be sent back to the client
-	 */
 	async fetch(request, env, ctx) {
-		// Isolate a single remote DO instance. Requests from workers will go there.
-		const stub = env.MY_DURABLE_OBJECT.getByName(new URL(request.url).pathname);
-
-		// Call an RPC method on the stub.
-		const greeting = await stub.sayHello("world");
-
-		return new Response(greeting);
+		return handler(request, Context.make(CfEnv, { env, ctx }));
 	},
 } satisfies ExportedHandler<Env>;
