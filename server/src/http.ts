ad788e7de7b6057b191a5d2c088f06446defe74b
Jack Morris <jack.morris@sharesies.co.nz>
2026-05-23 11:42:47 +1200

    refactor: extracted components like worker environment and durable namespace as services

--- server/src/http.ts
+++ server/src/http.ts
@@ -1,23 +1,20 @@
 import { HttpApiBuilder } from 'effect/unstable/httpapi';
 import { Api } from './api';
-import { Effect, Layer, Schema } from 'effect';
-import { HttpRouter, HttpServer, HttpServerResponse } from 'effect/unstable/http';
-import { CfEnv } from './services/cf-env';
+import { Effect, Layer } from 'effect';
+import { HttpRouter, HttpServer } from 'effect/unstable/http';
+import { DurableObjectNamespace } from './services/durable-object-namespace';
 
 
 const BaseLive = HttpApiBuilder.group(Api, 'base', (handlers) => handlers.handle('getUI', () => handleGetUi)).pipe(Layer.provide(HttpServer.layerServices));
 
 const handleGetUi = Effect.gen(function* () {
 	const { name } = yield* HttpRouter.params;
-	const { env } = yield* CfEnv;
 
-	if (name) {
-		const stub = env.MY_DURABLE_OBJECT.getByName(name);
-		const greeting = yield* Effect.promise(() => stub.sayHello('world'));
-        return { ui: ''}
-	}
+	const doNamespace = yield* DurableObjectNamespace;
+	const stub = yield* doNamespace.getByName(name!);
 
-    return HttpServerResponse.empty()
+	const greeting = yield* Effect.promise(() => stub.sayHello('world'));
+	return { ui: greeting };
 });
 
 export const ApiLive = HttpApiBuilder.layer(Api).pipe(Layer.provide(BaseLive));
