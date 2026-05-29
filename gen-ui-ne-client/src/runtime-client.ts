a1780af8a30da0057a5d69e643b9d2163aaf8461
Jack Morris <jack.morris@sharesies.co.nz>
2026-05-30 11:57:42 +1200

    feature: emit log from component, client starting to send logs to DO

--- gen-ui-ne-client/src/runtime-client.ts
+++ gen-ui-ne-client/src/runtime-client.ts
@@ -1,8 +1,9 @@
 import { ConfigProvider, Layer, ManagedRuntime } from "effect";
 import { Registry } from "./renderer/registry";
 import { Api, BuildApiUrl } from "./services/api";
+import { FetchHttpClient } from "effect/unstable/http";
 
-const ApiLayer = Api.Live.pipe(Layer.provide(BuildApiUrl.Live));
+const ApiLayer = Api.Live.pipe(Layer.provide(BuildApiUrl.Live), Layer.provide(FetchHttpClient.layer));
 
 const MainLayer = Layer.mergeAll(Registry.Live, ApiLayer).pipe(
   Layer.provide(
