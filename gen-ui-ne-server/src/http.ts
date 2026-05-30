81531bf8acc2901f83dd45cdb71bb6c7741d4f3d
slackermorris <35620369+slackermorris@users.noreply.github.com>
2026-05-31 10:39:10 +1200

    Support Log Ingestion (#20)
    * feature: endpoint for log ingestion plus implementation
    
    * feature: introduce logs table to sqlite
    
    * feature: can submit logs to sql storage on do instance
    
    * refactor: dto for ingest log endpoint should be declared inside the server project
    
    ---------
    
    Co-authored-by: Jack Morris <jack.morris@sharesies.co.nz>

--- gen-ui-ne-server/src/http.ts
+++ gen-ui-ne-server/src/http.ts
@@ -4,18 +4,35 @@ import { Effect, Layer, Schema } from 'effect';
 import { DurableObjectNamespace } from './services/durable-object-namespace';
 import { Api } from 'gen-ui-ne-shared/api';
 import { Spec } from 'gen-ui-ne-shared/model';
-
+import { OtlpLogRecordToLogInsertDto } from './models/dto';
 
 const BaseLive = HttpApiBuilder.group(Api, 'base', (handlers) =>
-	handlers.handle('getUI', ({ params }) => Effect.gen(function* () {
-		const doNamespace = yield* DurableObjectNamespace;
-		const stub = yield* doNamespace.getByName(params.name);
-		const raw = yield* Effect.promise(() => stub.getUi(params.name));
-		
+	handlers
+		.handle('getUI', ({ params }) =>
+			Effect.gen(function* () {
+				const doNamespace = yield* DurableObjectNamespace;
+				const stub = yield* doNamespace.getByName(params.name);
+				const raw = yield* Effect.promise(() => stub.getUi(params.name));
 
-		// TODO: simulate a decoding error 
-		return Schema.decodeUnknownSync(Spec)(raw)
-	}))
+				// TODO: simulate a decoding error
+				return Schema.decodeUnknownSync(Spec)(raw);
+			}),
+		)
+		.handle('log', ({ params, payload }) =>
+			Effect.gen(function* () {
+				// TODO: add some logging
+				const doNamespace = yield* DurableObjectNamespace;
+				const stub = yield* doNamespace.getByName(params.name);
+				const logs = Schema.decodeSync(Schema.Array(OtlpLogRecordToLogInsertDto))(payload.logs);
+
+
+				yield* Effect.promise(() => stub.ingestLogs(logs));
+
+				return {
+					ok: true,
+				};
+			}),
+		),
 );
 
 export const ApiLive = HttpApiBuilder.layer(Api).pipe(Layer.provide(BaseLive));
---
+++ gen-ui-ne-shared/api-schema.ts
@@ -0,0 +1,16 @@
+import { Schema, SchemaTransformation } from "effect"
+
+export const OtlpLogRecord = Schema.Struct({
+  timeUnixNano: Schema.optionalKey(Schema.String),
+  severityNumber: Schema.optionalKey(Schema.Number),
+  severityText: Schema.optionalKey(Schema.String),
+  body: Schema.String,
+  attributes: Schema.optionalKey(Schema.Array(Schema.Struct({
+    key: Schema.String,
+    value: Schema.Unknown,
+  }))),
+  traceId: Schema.optionalKey(Schema.String),
+  spanId: Schema.optionalKey(Schema.String),
+})
+
+export type OtlpLogRecord = typeof OtlpLogRecord.Type
